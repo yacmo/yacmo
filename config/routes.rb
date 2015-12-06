6ed71488efadc15f009e6ec41d63cc38f8ab43ea
kaname <kaname-f>
2015-12-06 22:43:23 +0900

    対象者用のログイン画面を作成しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'dashboards/index'
+
+  devise_for :users
+  resources :dashboards
+
+  root to: "dashboards#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
