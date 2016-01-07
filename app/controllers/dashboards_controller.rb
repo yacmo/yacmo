6f7b048bf87ca45f4111fa68014d09d20a2dbf90
kaname <kaname-f>
2016-01-07 20:00:51 +0900

    メッセージ履歴が存在しない場合の処理を追加

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,13 +3,15 @@ class DashboardsController < ApplicationController
   def index
     if !current_user.blank?
       # 最新のデータ
-      latest_data = current_user.user_data_histories.order(:created_at).last
-      # 最新の体重
-      @latest_body_mass = latest_data.body_mass
-      # 最新の歩数
-      @latest_steps = latest_data.steps
-      # メッセージの履歴
-      @message_histories = MessageHistory.all
+      if !current_user.user_data_histories.blank?
+        latest_data = current_user.user_data_histories.order(:created_at).last
+        # 最新の体重
+        @latest_body_mass = latest_data.body_mass
+        # 最新の歩数
+        @latest_steps = latest_data.steps
+        # メッセージの履歴
+        @message_histories = MessageHistory.all
+      end
     else
     end
   end
