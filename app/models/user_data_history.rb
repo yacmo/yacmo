7b6faec2fef1661c39f3c3172f268cccea9b85f9
kaname <kaname-f>
2016-01-09 18:36:49 +0900

    利用者データ履歴の更新処理を実装開始

--- app/models/user_data_history.rb
+++ app/models/user_data_history.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #  id         :integer  not null
 #  user_id   :integer  not null
 #  steps      :integer  not null
@@ -7,4 +8,24 @@
 
 class UserDataHistory < ActiveRecord::Base
   belongs_to :users
+
+  def update_user_data
+    config = begin
+      Fitgem::Client.symbolize_keys(YAML.load(File.opne("../../config/.figem.yml")))
+    rescue ArgumentError => e
+      puts "Cud not parse YAMLE: #{e.message}"
+    end
+
+    client = Fitgem::Client.new(config[:oauth])
+
+    if config[:oauth][:token] && config[:oauth][:secret]
+      begin
+        access_token = client.reconnect(config[:oauth][:token], config[:oauth][:secret])
+      rescue Exception => e
+        puts "Error: .fitgem.ymlのkeyが不正です。Fitgem::Clientへ再接続できません"
+      end
+    else
+      puts "Fitgem::Clientの初期化を行ってください"
+    end
+  end
 end
